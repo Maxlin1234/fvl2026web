d45697ce931b6d4e6b1279449d6092098ab56c4b
Warrick Tsai <windydodo@gmail.com>
2026-04-02 21:10:00 +0800

    修正網址轉址，並註解掉暫時用不到的路由。

--- src/router/index.js
+++ src/router/index.js
@@ -12,111 +12,107 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    // component: Fvlhome
-    component: () => import(/* webpackChunkName: 'about' */ '../components/Fvl2026Final.vue'),
+    redirect: '/festival/2026'
   },
-  {
-    path: '/fvl-fest-2024',
-    name: 'Fvlhome',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Fvlhome.vue'),
+  // {
+  //   path: '/fvl-fest-2024',
+  //   name: 'Fvlhome',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Fvlhome.vue'),
 
-  },
-  {
-    path: '/fvl-fest-2024_en',
-    name: 'Fvlhome_en',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Fvlhome_en.vue'),
-  },
-  {
+  // },
+  // {
+  //   path: '/fvl-fest-2024_en',
+  //   name: 'Fvlhome_en',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Fvlhome_en.vue'),
+  // },
+  // {
 
-    // path:'/Fvlhome/Aboutwork/:id/:title/:word/:photo/:photo2/:photo3/:artist',
-    path: '/fvl-fest-2024/Aboutwork/:id/:workId',
-    name: 'Aboutwork',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork.vue'),
-    meta: {
-      keepAlive: true, // 需要被缓存
-    },
-  },
-  {
+  //   // path:'/Fvlhome/Aboutwork/:id/:title/:word/:photo/:photo2/:photo3/:artist',
+  //   path: '/fvl-fest-2024/Aboutwork/:id/:workId',
+  //   name: 'Aboutwork',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork.vue'),
+  //   meta: {
+  //     keepAlive: true, // 需要被缓存
+  //   },
+  // },
+  // {
 
-    path: '/fvl-fest-2024/Aboutwork_All/:id/:workId',
-    name: 'Aboutwork_All',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_All.vue'),
-    meta: {
-      keepAlive: true, // 需要被缓存
-    },
-  },
+  //   path: '/fvl-fest-2024/Aboutwork_All/:id/:workId',
+  //   name: 'Aboutwork_All',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_All.vue'),
+  //   meta: {
+  //     keepAlive: true, // 需要被缓存
+  //   },
+  // },
 
-  {
+  // {
 
-    path: '/fvl-fest-2024/Aboutwork_en_All/:id/:workId',
-    name: 'Aboutwork_en_All',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_en_All.vue'),
-    meta: {
-      keepAlive: true, // 需要被缓存
-    },
-  },
-  {
+  //   path: '/fvl-fest-2024/Aboutwork_en_All/:id/:workId',
+  //   name: 'Aboutwork_en_All',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_en_All.vue'),
+  //   meta: {
+  //     keepAlive: true, // 需要被缓存
+  //   },
+  // },
+  // {
 
-    // path:'/Fvlhome/Aboutwork_en/:id/:title/:word/:photo/:photo2/:photo3/:artist',
-    path: '/fvl-fest-2024_en/Aboutwork_en/:id/:workId',
-    name: 'Aboutwork_en',
+  //   // path:'/Fvlhome/Aboutwork_en/:id/:title/:word/:photo/:photo2/:photo3/:artist',
+  //   path: '/fvl-fest-2024_en/Aboutwork_en/:id/:workId',
+  //   name: 'Aboutwork_en',
 
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_en.vue'),
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork_en.vue'),
 
-  },
+  // },
   // {
   //   path: '/Aboutwork',
   //   name: 'Aboutwork',
   //   component: () => import(/* webpackChunkName: 'about' */ '../views/Aboutwork.vue')
   // },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: 'about' */ '../views/About.vue'),
-  },
-  {
-    path: '/News',
-    name: 'News',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: 'about' */ '../views/News.vue'),
-  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/News',
+  //   name: 'News',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/News.vue'),
+  // },
 
-  {
-    path: '/All',
-    name: 'All',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/All.vue'),
-  },
-  {
-    path: '/All_en',
-    name: 'All_en',
-    component: () => import(/* webpackChunkName: 'about' */ '../views/All_en.vue'),
-  },
-  {
-    path: '/collection',
-    name: 'Collection',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: 'about' */ '../views/Collection.vue'),
-  },
+  // {
+  //   path: '/All',
+  //   name: 'All',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/All.vue'),
+  // },
+  // {
+  //   path: '/All_en',
+  //   name: 'All_en',
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/All_en.vue'),
+  // },
+  // {
+  //   path: '/collection',
+  //   name: 'Collection',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: 'about' */ '../views/Collection.vue'),
+  // },
   {
     path: '/festival',
     name: 'festival',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: 'about' */ '../components/Fvl2026Final.vue'),
+    redirect: '/festival/2026'
   },
   {
     path: '/festival/2026',
     name: 'fvl-fest-2026',
     alias: ['/fvl-fest-2026', '/festival/fvl-fest-2026'],
-    meta: { 
+    meta: {
       keepAlive: true,
       title: 'FUTURE VISION LAB 2026',
       metaTags: [
@@ -141,11 +137,11 @@ const routes = [
     // FvlFest2026.vue is under components, not views.
     component: () => import(/* webpackChunkName: 'fvl-fest-2026' */ '../components/Fvl2026Final.vue'),
   },
-  {
-    path: '/portfolioDetil',
-    name: 'PortfolioDetil',
-    component: () => import( '../views/PortfolioDetil.vue') // 注意檔名也建議改正
-  },
+  // {
+  //   path: '/portfolioDetil',
+  //   name: 'PortfolioDetil',
+  //   component: () => import( '../views/PortfolioDetil.vue') // 注意檔名也建議改正
+  // },
   {
     path: '/works/:id/:slug?',
     name: 'WorkDetail',
